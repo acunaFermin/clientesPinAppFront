93554b41d3a91145aa25d6f041fd90578ad643a4
Fermin Acuna <ferminacuna@medife.com.ar>
2025-05-15 22:45:33 -0300

    grid con mat card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { NuevoClienteComponent } from './nuevo-cliente/nuevo-cliente.component';
 import { ClientesKpiComponent } from './clientes-kpi/clientes-kpi.component';
 import { ListaClientesComponent } from './lista-clientes/lista-clientes.component';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
-  imports: [NuevoClienteComponent, ClientesKpiComponent, ListaClientesComponent], 
+  imports: [NuevoClienteComponent, ClientesKpiComponent, ListaClientesComponent, MatCardModule], 
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
